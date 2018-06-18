ac4ec772a02fd226f365ff50628d8f387e3254bd
jhassler1031 <jhassler1031@gmail.com>
2018-06-18 13:57:24 -0400

    cli now working for bands and albums, adding for tracks

--- record_store/record_store_client.py
+++ record_store/record_store_client.py
@@ -4,36 +4,61 @@ import requests
 bands_url = "http://localhost:8000/bands/"
 albums_url = "http://localhost:8000/albums/"
 tracks_url = "http://localhost:8000/tracks/"
+login_url = "http://localhost:8000/auth/token/create/"
 
 #Start Functions
 
-#Search for a band ==============================
+#Login
+def login(url):
+    username = input("Username: ")
+    password = input("Password: ")
+    auth_token = requests.post(url, data={"username": username, "password": password}).json()
+    auth_token = auth_token["auth_token"]
+    return auth_token
+
+#Search for something ==============================
 def search(url):
     while True:
         search = input("Enter query or press <enter> to go back: ")
         if search == "":
             return None
         else:
-            search_item = requests.get(url + search).json()
+            search_items = requests.get(url + search).json()
 
-        if search_item != []:
-            return search_item
-        else:
+        if search_items == []:
             print("No search results found")
+        else:
+            return search_items
 
-#Search sub menu
-def get_item_url(url):
-    item_id = input("Enter ID of entry to modify or Press <enter> to go back: ")
-    if user_input == "":
-        break
-    else:
-        item_url = url + item_id
-        return item_url
+def display_band(band):
+    print(f"""
+Band Name: {band["band_name"]}
+ID: {band["id"]}
+Genre: {band["genre"]}
+City Origin: {band["city_origin"]}
+Year Formed: {band["year_formed"]}
+""")
 
+def display_album(album):
+    print(f"""
+ID: {album["id"]}
+Title: {album["title"]}
+Genre: {album["genre"]}
+Release Year: {album["release_year"]}
+Notes: {album["notes"]}
+Band ID: {album["band"]}
+""")
+
+def display_track(track):
+    print("""
+ID: {track["id"]}
+Title: {track["title"]}
+Album ID: {track["album"]}
+    """)
 
 
 #Sub menu for band options ======================
-def band_menu(url):
+def band_menu(url, user):
     while True:
         print("""
 What would you like to do?
@@ -49,13 +74,7 @@ Press <enter> to go back
         if band_input == "1":
             bands = requests.get(url).json()
             for band in bands:
-                print(f"""
-Band Name: {band["band_name"]}
-ID: {band["id"]}
-Genre: {band["genre"]}
-City Origin: {band["city_origin"]}
-Year Formed: {band["year_formed"]}
-""")
+                display_band(band)
         #Add a band===============================
         elif band_input == "3":
             band_name = input("Band name: ")
@@ -63,37 +82,107 @@ Year Formed: {band["year_formed"]}
             city_origin = input("City Origin: ")
             year_formed = input("Year formed: ")
 
-            request.post(url, data={"band_name":band_name, "genre": genre, "city_origin": city_origin, "year_formed": year_formed})
+            requests.post(url,
+            data={"band_name":band_name, "genre": genre, "city_origin": city_origin, "year_formed": year_formed},
+            headers={"Authorization": "token " + user})
         #Go back =================================
         elif band_input == "":
             break
         #Ask for the band name to search =========
         else:
-            bands = search(url + "?band_name=")
-            if bands != None:
+            search_results = search(url + "?band_name=")
+            if search_results != None:
+                for band in search_results:
+                    display_band(band)
+                band_id = input("Enter the ID # to select or press <enter> to go back: ")
+                if band_id == "":
+                    break
+                else:
+                    band_url = url + band_id
+                    band = requests.get(band_url).json()
             #For just a search, print info =======
                 if band_input == "2":
-                    for band in bands:
-                        print(f"""
-ID: {band["id"]}
-Band Name: {band["band_name"]}
-Genre: {band["genre"]}
-City Origin: {band["city_origin"]}
-Year Formed: {band["year_formed"]}
-""")
+                    display_band(band)
                 #Update a band =========================
                 elif band_input == "4":
-                    band.band_name = input("Band name: ")
-                    band.genre = input("Genre: ")
-                    band.city_origin = input("City Origin: ")
-                    band.year_formed = input("Year formed: ")
-                    request.put(band_url, data={"band_name":band_name, "genre": genre, "city_origin": city_origin, "year_formed": year_formed})
+                    band_name = input("Band name: ")
+                    genre = input("Genre: ")
+                    city_origin = input("City Origin: ")
+                    year_formed = input("Year formed: ")
+                    requests.put(band_url,
+                    data={"band_name":band_name, "genre": genre, "city_origin": city_origin, "year_formed": year_formed},
+                    headers={"Authorization": "token " + user})
                 #Delete a band =========================
                 elif band_input == "5":
-                    requests.delete(band_url)
+                    requests.delete(band_url, headers={"Authorization": "token " + user})
+
+def album_menu(url, user):
+    while True:
+        print("""
+What would you like to do?
+1) Show all albums
+2) Search for an album
+3) Add an album
+4) Edit an album
+5) Delete an album
+Press <enter> to go back
+""")
+        album_input = input("> ")
+        #Show all bands ==========================
+        if album_input == "1":
+            albums = requests.get(url).json()
+            for album in albums:
+                display_album(album)
+        #Add a band===============================
+        elif album_input == "3":
+            title = input("Title: ")
+            genre = input("Genre: ")
+            release_year = input("Release Year: ")
+            notes = input("Notes: ")
+            band = input("Band ID: ")
+
+            requests.post(url,
+            data={"title":title, "genre": genre, "release_year": release_year, "notes": notes, "band": band},
+            headers={"Authorization": "token " + user})
+        #Go back =================================
+        elif album_input == "":
+            break
+        #Ask for the band name to search =========
+        else:
+            search_results = search(url + "?title=")
+            if search_results != None:
+                for album in search_results:
+                    display_album(album)
+                album_id = input("Enter the ID # to select or press <enter> to go back: ")
+                if album_id == "":
+                    break
+                else:
+                    album_url = url + album_id
+                    album = requests.get(album_url).json()
+            #For just a search, print info =======
+                if album_input == "2":
+                    display_album(album)
+                #Update a band =========================
+                elif album_input == "4":
+                    title = input("Title: ")
+                    genre = input("Genre: ")
+                    release_year = input("Release Year: ")
+                    notes = input("Notes: ")
+                    band = input("Band ID: ")
+
+                    requests.put(album_url,
+                    data={"title":title, "genre": genre, "release_year": release_year, "notes": notes, "band": band},
+                    headers={"Authorization": "token " + user})
+                #Delete a band =========================
+                elif album_input == "5":
+                    requests.delete(album_url, headers={"Authorization": "token " + user})
+
+
 
 
 #Start Program
+user = login(login_url)
+
 while True:
     print("""
 Choose a section:
@@ -104,10 +193,10 @@ Press <enter> to exit
 """)
     main_input = input("> ")
     if main_input == "1":
-        band_menu(bands_url)
+        band_menu(bands_url, user)
     elif main_input == "2":
-        pass
+        album_menu(albums_url, user)
     elif main_input == "3":
-        pass
+        track_menu(tracks_url, user)
     else:
         break
